58f8682372aa57f806cede1c402c96f86be3e1ed
Carl Wilson <carl@openpreservation.org>
2019-06-18 03:54:16 +0100

    First commit - skeleton project shape; - basic REST resources; - a few utilities; and - Maven POMs and `.gitignore`.

---
+++ jhove-rest-resources/src/main/java/org/openpreservation/jhove/rest/resources/ApiResource.java
@@ -0,0 +1,37 @@
+package org.openpreservation.jhove.rest.resources;
+
+import javax.ws.rs.Path;
+
+	
+/**
+ * @author  <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
+ *          <a href="https://github.com/carlwilson">carlwilson AT github</a>
+ *
+ * @version 0.1
+ */
+@Path("/")
+public final class ApiResource {
+
+	/**
+	 * 
+	 */
+	public ApiResource() {
+		super();
+	}
+
+    /**
+     * @return a new {@link ByteStreamResource}
+     */
+    @Path("/bytes/")
+    public static ByteStreamResource byteResources() {
+        return new ByteStreamResource();
+    }
+
+    /**
+     * @return a new {@link ByteStreamResource}
+     */
+    @Path("/validate/")
+    public static JhoveResources validationResources() {
+        return new JhoveResources();
+    }
+}
---
+++ jhove-rest-resources/src/main/java/org/openpreservation/jhove/rest/resources/JhoveResources.java
@@ -0,0 +1,82 @@
+/**
+ *
+ */
+package org.openpreservation.jhove.rest.resources;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+
+/**
+ * The REST resource definition for byte stream identification services, these
+ * are JERSEY REST services and it's the annotations that perform the magic of
+ * handling content types and serialisation.
+ *
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>.</p>
+ */
+public class JhoveResources {
+	private static final String SHA1_NAME = "SHA-1"; //$NON-NLS-1$
+    /**
+     * Default public constructor required by Jersey / Dropwizard
+     */
+    public JhoveResources() {
+        super();
+    }
+
+    /**
+     * @param uploadedInputStream
+     *            InputStream for the uploaded file
+     * @param contentDispositionHeader
+     *            extra info about the uploaded file, currently unused.
+     * @return the {@link org.verapdf.pdfa.metadata.bytestream.ByteStreamId} of
+     *         the uploaded file's byte stream serialised according to requested
+     *         content type.
+     */
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces({ MediaType.APPLICATION_JSON })
+    public static void validate(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") final FormDataContentDisposition contentDispositionHeader) throws IOException {
+        MessageDigest sha1 = getDigest();
+        DigestInputStream dis = new DigestInputStream(uploadedInputStream, sha1);
+    }
+
+    private static MessageDigest getDigest() {
+        try {
+            return MessageDigest.getInstance(SHA1_NAME);
+        } catch (NoSuchAlgorithmException nsaExcep) {
+            // If this happens the Java Digest algorithms aren't present, a
+            // faulty Java install??
+            throw new IllegalStateException(
+                    "No digest algorithm implementation for " + SHA1_NAME + ", check you Java installation.", nsaExcep); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+    }
+
+    private static Path inputToTempPath(InputStream is) throws IOException {
+        Path temp = Files.createTempFile("jhove-", "-up");
+        try (OutputStream os = new FileOutputStream(temp.toFile())) {
+            byte[] buff = new byte[4096];
+            int length;
+            while ((length = is.read(buff)) > 0) {
+                os.write(buff, 0, length);
+            }
+        }
+        return temp;
+    }
+}
